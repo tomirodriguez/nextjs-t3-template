42f0eefdab64e762eec3704608e1c7070d6d0258
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2023-08-14 10:03:01 -0300

    rename schema file

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,11 @@
 import { env } from "@/env.mjs";
 import { connect } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-import * as schemas from "./schemas";
+import * as schema from "./schema";
 
 // create the connection
 const connection = connect({
   url: env.DATABASE_URL,
 });
 
-export const db = drizzle(connection, { schema: { ...schemas } });
+export const db = drizzle(connection, { schema: { ...schema } });
